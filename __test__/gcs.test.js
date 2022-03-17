46111b2dbcf618c3887e391d5355c5ee31765162
Akira Kashihara <akira.kashihara@hotmail.com>
2022-03-17 22:07:42 +0900

    Test to establish GCS constructor.

--- __test__/gcs.test.js
+++ __test__/gcs.test.js
@@ -1,14 +1,15 @@
 import GCS from "../lib/gcs.js";
-
-const params = {
-  clientId: "clientId",
-  redirectUrl: "http://localhost:3000",
-  scope: "https://www.googleapis.com/auth/devstorage.read_only",
-};
+import ToolKit from "../lib/toolkit.js";
+const toolKit = new ToolKit();
 
 describe("[GCS] CONSTRUCTOR VALID TEST", () => {
   it("Assumed valid parameter", () => {
-    const gcs = new GCS(params);
+    const params = {
+      clientId: "clientId",
+      redirectUrl: "http://localhost:3000",
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
+    const gcs = new GCS(params, toolKit);
     expect(typeof gcs.oauthSignIn).toBe("function");
     expect(typeof gcs.uploadFiles).toBe("function");
     expect(typeof gcs.getAccessToken).toBe("function");
@@ -19,99 +20,335 @@ describe("[GCS] CONSTRUCTOR VALID TEST", () => {
 
 describe("[GCS] CONSTRUCTOR INVALID TEST", () => {
   it("Invalid input parameter, params.clientId is number", () => {
-    params.clientId = 3000;
+    const params = {
+      clientId: 3000,
+      redirectUrl: "http://localhost:3000",
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
     expect(() => {
-      const gcs = new GCS(params);
+      const gcs = new GCS(params, toolKit);
     }).toThrow(
       /^params.clientId is invalid value. You must set String value.$/
     );
   });
 
   it("Invalid input parameter, params.clientId is Boolean", () => {
-    params.clientId = true;
+    const params = {
+      clientId: true,
+      redirectUrl: "http://localhost:3000",
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
     expect(() => {
-      const gcs = new GCS(params);
+      const gcs = new GCS(params, toolKit);
     }).toThrow(
       /^params.clientId is invalid value. You must set String value.$/
     );
   });
 
   it("Invalid input parameter, params.clientId is null", () => {
-    params.clientId = null;
+    const params = {
+      clientId: null,
+      redirectUrl: "http://localhost:3000",
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
     expect(() => {
-      const gcs = new GCS(params);
+      const gcs = new GCS(params, toolKit);
     }).toThrow(
       /^params.clientId is invalid value. You must set String value.$/
     );
   });
 
   it("Invalid input parameter, params.clientId is undefined", () => {
+    const params = {};
     params.clientId;
+    params.redirectUrl = "http://localhost:3000";
+    params.scope = "https://www.googleapis.com/auth/devstorage.read_only";
+
     expect(() => {
-      const gcs = new GCS(params);
+      const gcs = new GCS(params, toolKit);
     }).toThrow(
       /^params.clientId is invalid value. You must set String value.$/
     );
   });
 
   it("Invalid input parameter, params.clientId is BigInt", () => {
-    params.clientId = BigInt(238298749019823849n);
+    const params = {
+      clientId: BigInt(238298749019823849n),
+      redirectUrl: "http://localhost:3000",
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
+
     expect(() => {
-      const gcs = new GCS(params);
+      const gcs = new GCS(params, toolKit);
     }).toThrow(
       /^params.clientId is invalid value. You must set String value.$/
     );
   });
 
   it("Invalid input parameter, params.clientId is Symbol", () => {
-    params.clientId = Symbol("hogehoge");
+    const params = {
+      clientId: Symbol("hogehoge"),
+      redirectUrl: "http://localhost:3000",
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
     expect(() => {
-      const gcs = new GCS(params);
+      const gcs = new GCS(params, toolKit);
     }).toThrow(
       /^params.clientId is invalid value. You must set String value.$/
     );
   });
 
   it("Invalid input parameter, params.clientId is Object", () => {
-    params.clientId = { hoge: "hogehoge" };
+    const params = {
+      clientId: { hoge: "hogehoge" },
+      redirectUrl: "http://localhost:3000",
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
     expect(() => {
-      const gcs = new GCS(params);
+      const gcs = new GCS(params, toolKit);
     }).toThrow(
       /^params.clientId is invalid value. You must set String value.$/
     );
   });
-  // it("Normal function test, only throw", () => {
-  //   expect(() => {
-  //     test.test();
-  //   }).toThrow();
-  // });
-  // it("Normal function test, match a part of error message", () => {
-  //   expect(() => {
-  //     test.test();
-  //   }).toThrow(/^Test.*/);
-  // });
-  // it("Normal function test, match a part of error message", () => {
-  //   expect(() => {
-  //     test.test();
-  //   }).toThrow("Test");
-  // });
-  // it("Normal function test, match a part of error message", () => {
-  //   expect(() => {
-  //     test.test();
-  //   }).toThrow(/^Test throw$/);
-  // });
-  // it("Normal function test, match error message", () => {
-  //   expect(() => {
-  //     test.test();
-  //   }).toThrow("Test throw");
-  // });
-  // it("Async function test, only throw", async () => {
-  //   await expect(test.asyncTest()).rejects.toThrow();
-  // });
-  // it("Async function test, match a part of error message", async () => {
-  //   await expect(test.asyncTest()).rejects.toThrow(/^test.*/);
-  // });
-  // it("Async function test, match error message", async () => {
-  //   await expect(test.asyncTest()).rejects.toThrow();
-  // });
+
+  it("Invalid input parameter, params.redirectUrl is number", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: 2000,
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
+
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.redirectUrl is invalid value. You must set string value which is an URL.$/
+    );
+  });
+
+  it("Invalid input parameter, params.redirectUrl is Boolean", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: true,
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
+
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.redirectUrl is invalid value. You must set string value which is an URL.$/
+    );
+  });
+
+  it("Invalid input parameter, params.redirectUrl is null", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: null,
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
+
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.redirectUrl is invalid value. You must set string value which is an URL.$/
+    );
+  });
+
+  it("Invalid input parameter, params.redirectUrl is undefined", () => {
+    const params = {};
+    params.clientId = "clientId";
+    params.redirectUrl;
+    params.scope = "https://www.googleapis.com/auth/devstorage.read_only";
+
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.redirectUrl is invalid value. You must set string value which is an URL.$/
+    );
+  });
+
+  it("Invalid input parameter, params.redirectUrl is BigInt", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: BigInt(38263749083234n),
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
+
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.redirectUrl is invalid value. You must set string value which is an URL.$/
+    );
+  });
+
+  it("Invalid input parameter, params.redirectUrl is Symbol", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: Symbol("hogehoge"),
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
+
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.redirectUrl is invalid value. You must set string value which is an URL.$/
+    );
+  });
+
+  it("Invalid input parameter, params.redirectUrl is String(not URL)", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: "http://hogehoge",
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
+
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.redirectUrl is invalid value. You must set string value which is an URL.$/
+    );
+  });
+
+  it("Invalid input parameter, params.redirectUrl is String(not URL)", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: "http://hogehoge",
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
+
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.redirectUrl is invalid value. You must set string value which is an URL.$/
+    );
+  });
+
+  it("Invalid input parameter, params.redirectUrl is Object", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: { hoge: "hoge" },
+      scope: "https://www.googleapis.com/auth/devstorage.read_only",
+    };
+
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.redirectUrl is invalid value. You must set string value which is an URL.$/
+    );
+  });
+
+  it("Invalid input parameter, params.scope is number", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: "http://localhost:3000",
+      scope: 300,
+    };
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.scope is invalid value. You must set string value which is a URL in scopes. A list of scopes is in https:\/\/cloud.google.com\/storage\/docs\/authentication.$/
+    );
+  });
+
+  it("Invalid input parameter, params.scope is Boolean", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: "http://localhost:3000",
+      scope: false,
+    };
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.scope is invalid value. You must set string value which is a URL in scopes. A list of scopes is in https:\/\/cloud.google.com\/storage\/docs\/authentication.$/
+    );
+  });
+
+  it("Invalid input parameter, params.scope is null", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: "http://localhost:3000",
+      scope: null,
+    };
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.scope is invalid value. You must set string value which is a URL in scopes. A list of scopes is in https:\/\/cloud.google.com\/storage\/docs\/authentication.$/
+    );
+  });
+
+  it("Invalid input parameter, params.scope is undefined", () => {
+    const params = {};
+    params.clientId = "clientId";
+    params.redirectUrl = "http://localhost:3000";
+    params.scope;
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.scope is invalid value. You must set string value which is a URL in scopes. A list of scopes is in https:\/\/cloud.google.com\/storage\/docs\/authentication.$/
+    );
+  });
+
+  it("Invalid input parameter, params.scope is BigInt", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: "http://localhost:3000",
+      scope: BigInt(92736382910384n),
+    };
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.scope is invalid value. You must set string value which is a URL in scopes. A list of scopes is in https:\/\/cloud.google.com\/storage\/docs\/authentication.$/
+    );
+  });
+
+  it("Invalid input parameter, params.scope is Symbol", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: "http://localhost:3000",
+      scope: Symbol("hoge"),
+    };
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.scope is invalid value. You must set string value which is a URL in scopes. A list of scopes is in https:\/\/cloud.google.com\/storage\/docs\/authentication.$/
+    );
+  });
+
+  it("Invalid input parameter, params.scope is String(not scope URL)", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: "http://localhost:3000",
+      scope: "https://hacknock.com",
+    };
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.scope is invalid value. You must set string value which is a URL in scopes. A list of scopes is in https:\/\/cloud.google.com\/storage\/docs\/authentication.$/
+    );
+  });
+
+  it("Invalid input parameter, params.scope is String(not URL)", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: "http://localhost:3000",
+      scope: "https://hacknock",
+    };
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.scope is invalid value. You must set string value which is a URL in scopes. A list of scopes is in https:\/\/cloud.google.com\/storage\/docs\/authentication.$/
+    );
+  });
+
+  it("Invalid input parameter, params.scope is Object", () => {
+    const params = {
+      clientId: "clientId",
+      redirectUrl: "http://localhost:3000",
+      scope: { hoge: 8888 },
+    };
+    expect(() => {
+      const gcs = new GCS(params, toolKit);
+    }).toThrow(
+      /^params.scope is invalid value. You must set string value which is a URL in scopes. A list of scopes is in https:\/\/cloud.google.com\/storage\/docs\/authentication.$/
+    );
+  });
 });
