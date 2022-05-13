22659eaff0e4ec02e1d504a7364b3a3c52e4946f
Akira Kashihara <akira.kashihara@hotmail.com>
2022-05-13 16:52:04 +0900

    Divide test file to each method of GCS.

--- __test__/getCount.js
+++
@@ -1,5 +0,0 @@
-export const getCount = async () => {
-  const response = await fetch("https://hogehoge.hogehoge.hogehoge"); // GETする
-  const countJson = await response.json(); // Responseのbodyをjsonで得る
-  return countJson.count; // 得られたjsonのcountの値を返す
-};
